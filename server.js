f439c0c017a52ae548fa1054e0e54e110d626903
Nafis Azizan <86115145+Nafizyo@users.noreply.github.com>
2022-07-12 03:46:11 +0800

    update server.js
    > some post & get routes structure
    > basic & db configs
    > start

--- server.js
+++ server.js
@@ -1,7 +1,41 @@
+// MODULES
 const express = require('express')
 const db = require(./db.js)
 const app = express()
-
-app.get('/', (req, res) => {
+;
+// BASIC CONFIG
+app.use(express.static('public'))
+app.use(express.urlencoded( {extended: false} ))
+app.use(express.json())
+;
+// DATABASE CONFIG
+  // import db
+const mongoose = require('mongoose')
+// const mySecret = process.env['MONGO_URI']
+//mongoose.connect(mySecret, {useNewUrlParser: true, useUnifiedTopology: true})
+  // schemas & models
+const { Schema } = mongoose
+;
+// POST ROUTES
+  // create new note
+app.post('/new/:title', (req, res) => {
   
-}
+})
+  // update past note
+app.post('/past/:title', (req, res) => {
+  
+})
+// GET ROUTES
+  // homepage
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + 'index.html')
+})
+  // get note by title
+app.get('/past/:title', (req, res) => {
+  
+})
+;
+// START
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
